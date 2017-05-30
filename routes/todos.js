28e9700c615d29c7b0e99c26afd3e84bbace6274
kgopi9k <kgopi9k@gmail.com>
2017-05-30 01:54:30 -0400

    Intial version

---
+++ routes/todos.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+var Todo = require('../models/todo.js');
+
+/* GET /todos listing. */
+router.get('/',ensureAuthenticated, function(req, res, next) {
+  Todo.find(function (err, todos) {
+    if (err) return next(err);
+    res.json(todos);
+  });
+});
+
+/* POST /todos */
+router.post('/',ensureAuthenticated, function(req, res, next) {
+  Todo.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET /todos/id */
+router.get('/:id',ensureAuthenticated, function(req, res, next) {
+  Todo.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* PUT /todos/:id */
+router.put('/:id',ensureAuthenticated, function(req, res, next) {
+  Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE /todos/:id */
+router.delete('/:id',ensureAuthenticated, function(req, res, next) {
+  Todo.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/users/login');
+	}
+}
+module.exports = router;
