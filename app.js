474b7fc7beee686ba771738a81f34f50ee9c24ca
kgopi9k <kgopi9k@gmail.com>
2017-05-31 01:47:11 -0400

    tt

--- app.js
+++ app.js
@@ -111,5 +111,5 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT ||3000;
  
 app.listen(port, ipaddress, function () {
-	console.info('Express server listening on port: ' + port);
+	console.info( 'Express server listening on port: ' + port);
 });
